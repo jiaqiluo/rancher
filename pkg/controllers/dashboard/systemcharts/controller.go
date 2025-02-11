c7c9d2bdb61520a3337f3bd5efdb76143c5b8444
Jiaqi Luo <jiaqi.luo@suse.com>
2025-02-11 13:36:44 -0700

    implement the feature imported-cluster-version-management

--- pkg/controllers/dashboard/systemcharts/controller.go
+++ pkg/controllers/dashboard/systemcharts/controller.go
@@ -10,13 +10,17 @@ import (
 	catalog "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
 	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/controllers/dashboard/chart"
+	"github.com/rancher/rancher/pkg/controllers/management/importedclusterversionmanagement"
 	"github.com/rancher/rancher/pkg/controllers/management/k3sbasedupgrade"
 	"github.com/rancher/rancher/pkg/features"
 	catalogcontrollers "github.com/rancher/rancher/pkg/generated/controllers/catalog.cattle.io/v1"
+	mgmtcontrollers "github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io/v3"
+	plancontrolers "github.com/rancher/rancher/pkg/generated/controllers/upgrade.cattle.io/v1"
 	v1 "github.com/rancher/rancher/pkg/generated/norman/core/v1"
 	"github.com/rancher/rancher/pkg/namespace"
 	"github.com/rancher/rancher/pkg/settings"
 	"github.com/rancher/rancher/pkg/wrangler"
+	upgradev1 "github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io/v1"
 	"github.com/rancher/wrangler/v3/pkg/data"
 	deploymentControllers "github.com/rancher/wrangler/v3/pkg/generated/controllers/apps/v1"
 	corecontrollers "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
@@ -25,15 +29,17 @@ import (
 	k8sappsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/util/retry"
 )
 
 const (
-	repoName           = "rancher-charts"
-	sucDeploymentName  = "system-upgrade-controller"
-	legacyAppLabel     = "io.cattle.field/appId"
-	legacyAppFinalizer = "systemcharts.cattle.io/legacy-k3s-based-upgrader-deprecation"
+	repoName             = "rancher-charts"
+	sucDeploymentName    = "system-upgrade-controller"
+	legacyAppLabel       = "io.cattle.field/appId"
+	legacyAppFinalizer   = "systemcharts.cattle.io/legacy-k3s-based-upgrader-deprecation"
+	managedPlanFinalizer = "systemcharts.cattle.io/rancher-managed-plan"
 )
 
 var (
@@ -58,6 +64,8 @@ func Register(ctx context.Context, wContext *wrangler.Context, registryOverride 
 		deployment:       wContext.Apps.Deployment(),
 		deploymentCache:  wContext.Apps.Deployment().Cache(),
 		clusterRepo:      wContext.Catalog.ClusterRepo(),
+		cluster:          wContext.Mgmt.Cluster(),
+		plan:             wContext.Plan.Plan(),
 		chartsConfig:     chart.RancherConfigGetter{ConfigCache: wContext.Core.ConfigMap().Cache()},
 		registryOverride: registryOverride,
 	}
@@ -71,6 +79,10 @@ func Register(ctx context.Context, wContext *wrangler.Context, registryOverride 
 	relatedresource.WatchClusterScoped(ctx, "bootstrap-configmap-charts", relatedConfigMaps, wContext.Catalog.ClusterRepo(), wContext.Core.ConfigMap())
 
 	wContext.Apps.Deployment().OnChange(ctx, "legacy-k3sBasedUpgrader-deprecation", h.onDeployment)
+
+	wContext.Plan.Plan().OnChange(ctx, "monitor-plans", h.onPlan)
+
+	wContext.Mgmt.Cluster().OnChange(ctx, "monitor-local-cluster", h.onCluster)
 	return nil
 }
 
@@ -79,8 +91,10 @@ type handler struct {
 	namespaces       corecontrollers.NamespaceController
 	deployment       deploymentControllers.DeploymentController
 	deploymentCache  deploymentControllers.DeploymentCache
+	plan             plancontrolers.PlanController
 	clusterRepo      catalogcontrollers.ClusterRepoController
 	chartsConfig     chart.RancherConfigGetter
+	cluster          mgmtcontrollers.ClusterController
 	registryOverride string
 }
 
@@ -102,11 +116,10 @@ func (h *handler) onRepo(key string, repo *catalog.ClusterRepo) (*catalog.Cluste
 		systemGlobalRegistry["cattle"] = registryMap
 	}
 	for _, chartDef := range h.getChartsToInstall() {
-		if chartDef.Enabled != nil && !chartDef.Enabled() {
-			continue
-		}
-
 		if chartDef.Uninstall {
+			logrus.Infof("===== attempting to uninstall the chart %s", chartDef.ChartName) // TODO: remove the logging
+			// it is important to remove the chat from the desired chart list
+			h.manager.Remove(chartDef.ReleaseNamespace, chartDef.ChartName)
 			if err := h.manager.Uninstall(chartDef.ReleaseNamespace, chartDef.ChartName); err != nil {
 				return repo, err
 			}
@@ -117,6 +130,10 @@ func (h *handler) onRepo(key string, repo *catalog.ClusterRepo) (*catalog.Cluste
 			}
 			continue
 		}
+		if chartDef.Enabled != nil && !chartDef.Enabled() {
+			continue
+		}
+		logrus.Infof("===== attempting to install the chart %s", chartDef.ChartName) // TODO: remove the logging
 
 		values := map[string]interface{}{
 			"global": systemGlobalRegistry,
@@ -240,11 +257,48 @@ func (h *handler) getChartsToInstall() []*chart.Definition {
 						toEnable = true
 					}
 				}
-				logrus.Infof("[systemcharts] feature ManagedSystemUpgradeController = %t, toEnable = %t",
-					features.ManagedSystemUpgradeController.Enabled(), toEnable)
-
-				return features.ManagedSystemUpgradeController.Enabled() && toEnable
+				toInstall := features.ManagedSystemUpgradeController.Enabled() && toEnable
+				logrus.Debugf("[systemcharts] install system-upgrade-controller: %t (ManagedSystemUpgradeController: %t && toEnable: %t)",
+					toInstall, features.ManagedSystemUpgradeController.Enabled(), toEnable)
+				return toInstall
 			},
+			Uninstall: func() bool {
+				// The absence of rancher-managed plans indicates the cluster is ready for uninstalling the app.
+				// The removal of the plans is handled in the k3sbasedupgrade package.
+				noManagedPlan := false
+				set := labels.Set(map[string]string{k3sbasedupgrade.RancherManagedPlan: "true"})
+				plans, err := h.plan.Cache().List(namespace.System, set.AsSelector())
+				if err != nil {
+					logrus.Warnf("[systemcharts] failed to list plans: %v", err)
+				}
+				if len(plans) == 0 {
+					noManagedPlan = true
+				}
+
+				var versionManagementEnabled bool
+				if features.MCMAgent.Enabled() {
+					// For the imported RKE2/K3s cluster,
+					// the ManagedSystemUpgradeController feature is configured during the installation or update
+					// of the cluster agent to indicate the activation of the version management feature within the cluster.
+					// This logic runs in the cluster agent.
+					versionManagementEnabled = features.ManagedSystemUpgradeController.Enabled()
+				}
+				if features.MCM.Enabled() {
+					// for a local cluster,
+					// Rancher has direct access to the mgmt v3 cluster and the ImportedClusterVersionManagement feature
+					cluster, err := h.cluster.Get("local", metav1.GetOptions{})
+					if err != nil {
+						logrus.Warnf("[systemcharts] failed to get the local cluster: %v", err)
+					}
+					if cluster != nil && (cluster.Status.Driver == v3.ClusterDriverRke2 || cluster.Status.Driver == v3.ClusterDriverK3s) {
+						versionManagementEnabled = importedclusterversionmanagement.VersionManagementEnabled(cluster)
+					}
+				}
+				toUninstall := !versionManagementEnabled && noManagedPlan
+				logrus.Debugf("[systemcharts] uninstall system-upgrade-controlle: %t (!versionManagementEnabled: %t && noManagedPlan: %t)",
+					toUninstall, !versionManagementEnabled, noManagedPlan)
+				return toUninstall
+			}(),
 		},
 	}
 }
@@ -260,7 +314,7 @@ func (h *handler) onDeployment(_ string, d *k8sappsv1.Deployment) (*k8sappsv1.De
 		return d, nil
 	}
 	index := slices.Index(d.Finalizers, legacyAppFinalizer)
-	logrus.Infof("[systemcharts] found deployment %s/%s with label %s=%s, index of target finalzier = %d",
+	logrus.Debugf("[systemcharts] found deployment %s/%s with label %s=%s, index of target finalzier = %d",
 		d.Namespace, d.Name, legacyAppLabel, d.Labels[legacyAppLabel], index)
 	if (d.DeletionTimestamp != nil && index == -1) || (d.DeletionTimestamp == nil && index >= 0) {
 		return d, nil
@@ -316,6 +370,92 @@ func (h *handler) onDeployment(_ string, d *k8sappsv1.Deployment) (*k8sappsv1.De
 		return d, nil
 	}
 	return d, nil
+}
+
+// onPlan manages the finalizers on rancher-managed Plan,
+// it enqueues the rancher-charts ClusterRepo to the controller's processing queue when the Plan is deleted.
+func (h *handler) onPlan(_ string, plan *upgradev1.Plan) (*upgradev1.Plan, error) {
+	if plan == nil || plan.Namespace != namespace.System || plan.Labels[k3sbasedupgrade.RancherManagedPlan] != "true" {
+		return plan, nil
+	}
+	index := slices.Index(plan.Finalizers, managedPlanFinalizer)
+	logrus.Debugf("[systemcharts] found plan %s/%s with index of target finalzier = %d", plan.Namespace, plan.Name, index)
+	if (plan.DeletionTimestamp != nil && index == -1) || (plan.DeletionTimestamp == nil && index >= 0) {
+		return plan, nil
+	}
+	var err error
+	switch {
+	case plan.DeletionTimestamp != nil && index >= 0:
+		// When the plan is being deleted, remove the finalizer if it exists, and enqueue the rancher-charts clusterRepo
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			plan, err = h.plan.Get(plan.Namespace, plan.Name, metav1.GetOptions{})
+			if err != nil {
+				if errors.IsNotFound(err) {
+					return nil
+				}
+				return err
+			}
+			if plan == nil {
+				return nil
+			}
+			index := slices.Index(plan.Finalizers, managedPlanFinalizer)
+			if index == -1 {
+				return nil
+			}
+			plan = plan.DeepCopy()
+			plan.Finalizers = append(plan.Finalizers[:index], plan.Finalizers[index+1:]...)
+			plan, err = h.plan.Update(plan)
+			return err
+		}); err != nil {
+			return nil, fmt.Errorf("failed to update plan %s/%s: %w", plan.Namespace, plan.Name, err)
+		}
+
+		logrus.Infof("[systemcharts] enqueue %s", repoName)
+		h.clusterRepo.EnqueueAfter(repoName, 2*time.Second)
+
+	case plan.DeletionTimestamp == nil && index == -1:
+		// If the plan is not being deleted, add the finalizer if it is absent to ensure Wrangler can detect the deletion event
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			plan, err = h.plan.Get(plan.Namespace, plan.Name, metav1.GetOptions{})
+			if err != nil {
+				if errors.IsNotFound(err) {
+					return nil
+				}
+				return err
+			}
+			if plan == nil {
+				return nil
+			}
+			index := slices.Index(plan.Finalizers, managedPlanFinalizer)
+			if index >= 0 {
+				return nil
+			}
+			plan = plan.DeepCopy()
+			plan.Finalizers = append(plan.Finalizers, managedPlanFinalizer)
+			plan, err = h.plan.Update(plan)
+			return err
+		}); err != nil {
+			return nil, fmt.Errorf("failed to update plan %s/%s: %w", plan.Namespace, plan.Name, err)
+		}
+	default:
+		return plan, nil
+	}
+	return plan, nil
+}
+
+// OnCluster enqueues the rancher-charts ClusterRepo to the controller's processing queue when the local cluster is updated.
+// It ensures the timely installation or uninstallation of the system-upgrade-controller app in the local cluster
+// when the "rancher.io/imported-cluster-version-management" annotation is changed.
+func (h *handler) onCluster(_ string, obj *v3.Cluster) (*v3.Cluster, error) {
+	if !features.MCM.Enabled() {
+		return obj, nil
+	}
+	if obj == nil || obj.DeletionTimestamp != nil || obj.Name != "local" {
+		return obj, nil
+	}
+	logrus.Debugf("[systemcharts] local cluster has been updated, enqueue %s", repoName)
+	h.clusterRepo.EnqueueAfter(repoName, 2*time.Second)
+	return obj, nil
 }
 
 // setPriorityClass attempts to retrieve the priority class for rancher pods and set it in the specified map
---
+++ pkg/controllers/management/importedclusterversionmanagement/imported_cluster_version_management.go
@@ -0,0 +1,33 @@
+package importedclusterversionmanagement
+
+import (
+	mgmtv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	"github.com/rancher/rancher/pkg/features"
+)
+
+const (
+	// VersionManagementAnno indicates whether the version management is enabled for a cluster.
+	// It defines the cluster-level behavior and takes precedence over the 'imported-cluster-version-management' feature.
+	// If absent on the cluster object, the value of the 'imported-cluster-version-management' feature will be used.
+	// It is only recognized on imported RKE2/K3s clusters and the local cluster if it is an RKE2/k3s cluster.
+	// It is ignored if found on a mgmt v3 cluster for other types of clusters.
+	// Expected values: "true", "false", or "system-default" (type: string)
+	VersionManagementAnno = "rancher.io/imported-cluster-version-management"
+)
+
+// VersionManagementEnabled checks if version management is enabled for a given cluster
+func VersionManagementEnabled(cluster *mgmtv3.Cluster) bool {
+	if cluster == nil {
+		return false
+	}
+	switch cluster.Annotations[VersionManagementAnno] {
+	case "true":
+		return true
+	case "false":
+		return false
+	case "system-default":
+		return features.ImportedClusterVersionManagement.Enabled()
+	default:
+		return features.ImportedClusterVersionManagement.Enabled()
+	}
+}
