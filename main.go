a274bedbcdfe5034db411a2ef725fdcc05169d5a
Jiaqi Luo <jiaqi.luo@suse.com>
2026-06-25 17:39:59 -0700

    make Linode node driver non-builtin
    - switch Linode node driver from embedded binary to URL
    - add one-time migration to disable unused Linode node driver and clean up related resources

--- main.go
+++ main.go
@@ -209,6 +209,9 @@ func initLogs(c *cli.Context, cfg rancher.Options) {
 		logrus.SetFormatter(&logrus.JSONFormatter{})
 	}
 	logrus.SetOutput(os.Stdout)
+	// for testing, always enable debug
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.Debugf("Loglevel set to [%v]", logrus.DebugLevel)
 	if cfg.Debug {
 		logrus.SetLevel(logrus.DebugLevel)
 		logrus.Debugf("Loglevel set to [%v]", logrus.DebugLevel)
